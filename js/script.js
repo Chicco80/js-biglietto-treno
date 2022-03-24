8e60a4608eed831b58d0def3d6a8130766a7f909
Chicco80 <chicco80lisa@gmail.com>
2022-03-24 15:26:08 +0100

    inizio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let kilometer = parseInt(prompt("Quanti km devi fare?"));
+console.log(kilometer); 
+
+let age = parseInt(prompt("quanti anni hai?"));
+console.log(age);
+
+let price = 0.21;
+
+let total = kilometer * price;
+console.log(total);
