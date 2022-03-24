eeb31a67a5e92283253342bbcb90cdcb29cae139
Chicco80 <chicco80lisa@gmail.com>
2022-03-24 17:54:56 +0100

    da modif

--- js/script.js
+++ js/script.js
@@ -4,7 +4,35 @@ console.log(kilometer);
 let age = parseInt(prompt("quanti anni hai?"));
 console.log(age);
 
-let price = 0.21;
+const price = 0.21;
 
 let total = kilometer * price;
 console.log(total);
+
+
+const giovani = ((total * 20) / 100);
+console.log(giovani);
+
+ const anziani = ((total * 40) / 100);
+
+if (age < 18){
+
+    total = (total - giovani);
+    console.log("meno");
+
+} else if  (age > 65){
+
+    total = (total - anziani);
+    console.log("piu");
+
+
+} else {
+
+    total = (total);
+    console.log("norm");
+}
+
+ let biglietto = document.getElementById("prezzoBiglietto").innerHTML += total + "£";
+ 
+
+
